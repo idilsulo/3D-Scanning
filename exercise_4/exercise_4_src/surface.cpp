2628605ae75555351de3ccc35af62e0ba7a10303
zushu <zumrudshukurlu@gmail.com>
2021-05-23 02:04:34 +0200

    surface solution added

--- exercise_4/exercise_4_src/surface.cpp
+++ exercise_4/exercise_4_src/surface.cpp
@@ -8,7 +8,22 @@
 // TODO: Implement the cost function
 struct SurfaceCostFunction
 {
+    SurfaceCostFunction(const Point3D& point_)
+            : point(point_)
+    {
+    }
 
+    template<typename T>
+    bool operator()(const T* const a, const T* const b, const T* const c, T* residual) const
+    {
+
+        residual[0] = point.z - (pow(point.x, 2.0)/a[0] - pow(point.y, 2.0)/b[0])/c[0];
+        return true;
+
+    }
+
+private:
+    const Point3D point;
 };
 
 
@@ -17,7 +32,7 @@ int main(int argc, char** argv)
 	google::InitGoogleLogging(argv[0]);
 
 	// Read 3D surface data points and define the parameters of the problem
-	const std::string file_path = "../data/points_surface.txt";
+	const std::string file_path = "../../data/points_surface.txt";
 	const auto points = read_points_from_file<Point3D>(file_path);
 	
 	const double a_initial = 1.0;
@@ -31,7 +46,15 @@ int main(int argc, char** argv)
 	ceres::Problem problem;
 
 	// TODO: For each data point create one residual block
-	
+    for (auto& point : points)
+    {
+        problem.AddResidualBlock(
+                new ceres::AutoDiffCostFunction<SurfaceCostFunction, 1, 1, 1, 1>(
+                        new SurfaceCostFunction(point)
+                ),
+                nullptr, &a, &b, &c
+        );
+    }
 
 	ceres::Solver::Options options;
 	options.max_num_iterations = 100;
